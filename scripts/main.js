54b0ee64d06ad9bdcd4cfb496ac7c621072cc7f4
吳承翰 <wu66415@gmail.com>
2022-12-16 16:05:08 +0800

    1216

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "以下都是沒用的東西~~";
+function toggle_info() {
+  const foo = document.getElementById("foo");
+  foo.classList.toggle("invisible");
+}
